dea9a1e4d1cda698a078fd5158f3fe91610b7185
Amejonah1200 <amejonah@gmail.com>
2020-02-26 23:25:46 +0100

    Added some Labels and a Button (Play/Pause), and made checkboxes working  more efficiently, and renamed the scale variable

--- src/main/java/ap/amejonah/sinusapp/SinusApp.java
+++ src/main/java/ap/amejonah/sinusapp/SinusApp.java
@@ -3,9 +3,12 @@ package ap.amejonah.sinusapp;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
-import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
@@ -14,22 +17,21 @@ import javafx.stage.Stage;
 
 public class SinusApp extends Application {
   
-  public static void main(String[] args) {
+  public static void launchApp(String[] args) {
     launch(args);
   }
   
   private Stage primaryStage;
   private AnimationController animationController;
   private List<GraphFunction> functions = new ArrayList<>();
-  private int startSearchBoxes;
   
   @Override
   public void start(Stage stage) throws Exception {
     // Initialization of the functions
-    functions.add(new SinusFunction("sin(x)", 1, 1, 0, new Color(1, 0.5, 0, 1)));
-    functions.add(new SinusFunction("0.75*sin(x)", 0.75, 1, 0, new Color(1, 0, 1, 1)));
-    functions.add(new SinusFunction("sin(x + PI/2)", 1, 1, Math.PI / 2, new Color(1, 0.5, 0, 1)));
-    functions.add(new CosinusFunction("-cos(x)", -1, 1, 0, new Color(1, 0.5, 0, 1)));
+    functions.add(new SinusFunction("Stromstärke", 0.75, 1, 0, new Color(1, 0, 1, 1)));
+    functions.add(new SinusFunction("Ohmscher Widerstand", 1, 1, 0, new Color(1, 0.5, 0, 1)));
+    functions.add(new SinusFunction("Induktiver Widerstand", 1, 1, Math.PI / 2, new Color(1, 0.5, 0, 1)));
+    functions.add(new SinusFunction("Kapazitiver Widerstand", 1, 1, Math.PI, new Color(1, 0.5, 0, 1)));
     // ---
     primaryStage = stage;
     AnchorPane pane = new AnchorPane();
@@ -38,7 +40,7 @@ public class SinusApp extends Application {
     pane.setPrefWidth(1000);
     pane.setMinWidth(1000);
     // setting up global values
-    int scaleGlobal = 125;
+    int yScale = 125;
     int startX = 450;
     int startY = 300;
     int length = 500;
@@ -53,8 +55,8 @@ public class SinusApp extends Application {
     // length - 1 lines and 1 arc -> 500 children for 1 function
     for (SimpleMathFunction function : functions) {
       for (int x = 0; x < length - 1; x++) {
-        funcLine = new Line(startX + x, startY - (function.f(xScale * x)) * scaleGlobal, startX + x + 1,
-            startY - (function.f(xScale * x + xScale)) * scaleGlobal);
+        funcLine = new Line(startX + x, startY - (function.f(xScale * x)) * yScale, startX + x + 1,
+            startY - (function.f(xScale * x + xScale)) * yScale);
         funcLine.strokeWidthProperty().set(2);
         funcLine.setStroke(((GraphFunction) function).getColor());
         funcLine.setId("line:" + id);
@@ -62,14 +64,15 @@ public class SinusApp extends Application {
       }
       double coef = function instanceof GraphFunction ? ((GraphFunction) function).getCoefficientGlobal() : 1;
       functionArc = new Arc();
-      functionArc.setRadiusX(scaleGlobal * coef);
-      functionArc.setRadiusY(scaleGlobal * coef);
+      functionArc.setRadiusX(yScale * coef);
+      functionArc.setRadiusY(yScale * coef);
       functionArc.setStartAngle(0);
       functionArc.setLength(375);
       functionArc.setLayoutX(200);
       functionArc.setLayoutY(startY);
       functionArc.setFill(new Color(0, 0, 0, 0));
       functionArc.setStroke(((GraphFunction) function).getColor());
+      functionArc.setId("arc:" + id);
       pane.getChildren().add(functionArc);
       id++;
     }
@@ -78,14 +81,14 @@ public class SinusApp extends Application {
     Line xAxis = new Line(startX - 20, startY, startX + length - 1, startY);
     xAxis.setStroke(new Color(0.25, 0.25, 0.25, 1));
     pane.getChildren().add(xAxis);
-    Line yAxis = new Line(startX, startY - scaleGlobal - 20, startX, startY + scaleGlobal + 20);
+    Line yAxis = new Line(startX, startY - yScale - 20, startX, startY + yScale + 20);
     yAxis.setStroke(new Color(0.25, 0.25, 0.25, 1));
     pane.getChildren().add(yAxis);
     // ---
     // setting up the main arc and the 90° arc
     Arc arc = new Arc();
-    arc.setRadiusX(scaleGlobal);
-    arc.setRadiusY(scaleGlobal);
+    arc.setRadiusX(yScale);
+    arc.setRadiusY(yScale);
     arc.setStartAngle(0);
     arc.setLength(360);
     arc.setLayoutX(200);
@@ -108,16 +111,29 @@ public class SinusApp extends Application {
     // ---
     // ---------------------------
     // place for adding other children without affecting the animation
+    pane.getChildren().add(createLabel("90°", 205, startY - 25, 1));
+    pane.getChildren().add(createLabel("1", 205, startY - yScale - 20, 1.5D));
+    pane.getChildren().add(createLabel("-1", 205, startY + yScale + 10, 1.5D));
+    pane.getChildren().add(createLabel("1", 200 + yScale + 10, startY, 1.5D));
+    pane.getChildren().add(createLabel("-1", 200 - yScale - 15, startY, 1.5D));
+    pane.getChildren().add(createLabel("1", startX - 10, startY - yScale - 10, 1.5D));
+    pane.getChildren().add(createLabel("0", startX - 10, startY, 1.5D));
+    pane.getChildren().add(createLabel("π/2", startX + (length + 10) / 2, startY, 1.5D));
+    pane.getChildren().add(createLabel("2π", startX + length + 5, startY, 1.5D));
+    pane.getChildren().add(createLabel("-1", startX - 15, startY + yScale - 10, 1.5D));
+    pane.getChildren().add(createButton("▶", 30, 955, 15, (event) -> {
+      animationController.setRunning(!animationController.isRunning());
+      if (animationController.isRunning()) ((Button) event.getSource()).setText("⏸");
+      else ((Button) event.getSource()).setText("▶");
+    }));
     // ---------------------------
     // setting up the checkboxes for enabling and disabling the lines and arcs
-    startSearchBoxes = pane.getChildren().size();
     CheckBox checkBox;
     for (int i1 = 0; i1 < functions.size(); i1++) {
-      checkBox = new CheckBox(" -> Funktion: " + functions.get(i1).getName());
+      checkBox = new CheckBox(" -> " + functions.get(i1).getName());
       checkBox.setId("box:" + i1);
-      checkBox.setOnAction((event) -> {
-        updateLines();
-      });
+      checkBox.setOnAction((
+          event) -> updateLines(Integer.parseInt(((CheckBox) event.getSource()).getId().split(":")[1]), ((CheckBox) event.getSource()).isSelected()));
       checkBox.setLayoutX(14);
       checkBox.setSelected(true);
       checkBox.setLayoutY(14 + 30 * i1);
@@ -125,10 +141,10 @@ public class SinusApp extends Application {
     }
     // ---
     // setting up the x and y axis for the main arc
-    xAxis = new Line(200 - scaleGlobal - 20, startY, 200 + scaleGlobal + 20, startY);
+    xAxis = new Line(200 - yScale - 20, startY, 200 + yScale + 20, startY);
     xAxis.setStroke(new Color(0.25, 0.25, 0.25, 1));
     pane.getChildren().add(xAxis);
-    yAxis = new Line(200, startY - scaleGlobal - 20, 200, startY + scaleGlobal + 20);
+    yAxis = new Line(200, startY - yScale - 20, 200, startY + yScale + 20);
     yAxis.setStroke(new Color(0.25, 0.25, 0.25, 1));
     pane.getChildren().add(yAxis);
     // ---
@@ -137,35 +153,41 @@ public class SinusApp extends Application {
     stage.setScene(scene);
     stage.sizeToScene();
     stage.setResizable(false);
-    stage.setTitle("Zeigerdiagramm App");
+    stage.setTitle("Phasenverschiebung im Wechselstromkreis");
     stage.show();
     // ---
     // creating the AnimationController and start it
-    animationController = new AnimationController(this, startX, length, scaleGlobal, xScale);
+    animationController = new AnimationController(this, startX, length, yScale, xScale);
     animationController.animate();
     // ---
     // setting that on close it must stop the ScheduledExcutorService
     stage.setOnCloseRequest((event) -> Runtime.getRuntime().exit(0));
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      
-      public void run() {
-        animationController.terminate();
-      }
-    });
     // ---
   }
   
-  private void updateLines() {
-    int lineId;
-    boolean visible;
-    for (Node node : ((AnchorPane) primaryStage.getScene().getRoot()).getChildren())
-      if (node.getId() != null && node.getId().startsWith("line")) {
-        lineId = Integer.parseInt(node.getId().split(":")[1]);
-        visible = ((CheckBox) ((AnchorPane) primaryStage.getScene().getRoot()).getChildren().get(startSearchBoxes
-            + lineId)).isSelected();
-        node.setVisible(visible);
-        animationController.setVisible(functions.size() - lineId - 1, visible);
-      }
+  private Button createButton(String name, int length, int posX, int posY, EventHandler<ActionEvent> action) {
+    Button button = new Button(name);
+    button.setLayoutX(posX);
+    button.setLayoutY(posY);
+    button.setPrefSize(length, 25);
+    button.setOnAction(action);
+    return button;
+  }
+  
+  private Label createLabel(String name, int layoutX, int layoutY, double scale) {
+    Label tempLabel = new Label(name);
+    tempLabel.setLayoutX(layoutX);
+    tempLabel.setLayoutY(layoutY);
+    tempLabel.setScaleX(scale);
+    tempLabel.setScaleY(scale);
+    return tempLabel;
+  }
+  
+  private void updateLines(int id, boolean visible) {
+    for (int i = id * 500; i < id * 500 + 500; i++) {
+      ((AnchorPane) primaryStage.getScene().getRoot()).getChildren().get(i).setVisible(visible);
+    }
+    animationController.setVisible(functions.size() - id - 1, visible);
   }
   
   public Stage getPrimaryStage() {
